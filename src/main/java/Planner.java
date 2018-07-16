4a190100c68c947a1d6c6c72ad35323889e38f00
Aleksandr Matjushenko <guevara3647@gmail.com>
2018-07-17 00:38:49 +0300

    Планировщик заданий. Version 1.2

--- src/main/java/Planner.java
+++ src/main/java/Planner.java
@@ -11,7 +11,6 @@ class Planner {
     private Map<Integer, Set<Integer>> uniqueTasksMap = new HashMap<>(); // Карта <Уникальная задача, Зависящие от нее задачи>
     private Set<Integer> dependedTasksSet = new HashSet<>(); // Список уникальных зависящих задач
     private Set<Integer> highestPriorityTasks = new HashSet<>();//список уникальных задач наивысшего приоритета
-    private Set<Integer> uniqueTasksSet = new HashSet<>();//список уникальных задач
 
     void planTasks(String file) {
         // Считываем построчно файл и заполняем коллекции
@@ -20,40 +19,35 @@ class Planner {
                 String[] values = s.split(" ");
                 firstNumber = Integer.parseInt(values[0]);
                 secondNumber = Integer.parseInt(values[1]);
-                if(!uniqueTasksMap.containsKey(firstNumber)) {
+                if (!uniqueTasksMap.containsKey(firstNumber)) {
                     uniqueTasksMap.put(firstNumber, new HashSet<>());
                 }
                 uniqueTasksMap.get(firstNumber).add(secondNumber);
                 dependedTasksSet.add(secondNumber);
-                uniqueTasksSet.add(firstNumber);
-                uniqueTasksSet.add(secondNumber);
+                highestPriorityTasks.add(firstNumber);
             }
         } catch (IOException exc) {
             System.out.println("Ошибка чтения файла: " + exc);
         }
+        highestPriorityTasks.removeAll(dependedTasksSet);
         // Пока список уникадьных задач не пуст
         while (!uniqueTasksMap.isEmpty()) {
-            //Ищем задачи высшего приоритета
-            highestPriorityTasks.addAll(uniqueTasksSet);
-            highestPriorityTasks.removeAll(dependedTasksSet);
-
+            //Выводим на консоль задачи с высшим приоритетом
+            System.out.println(highestPriorityTasks);
             //Очищаем коллекции от задач наивысшего приоритета
             for (Integer highestPriority : highestPriorityTasks) {
                 uniqueTasksMap.remove(highestPriority);
             }
-            uniqueTasksSet.removeAll(highestPriorityTasks);
-
             //Создаем новый список зависящих задач
             if(!uniqueTasksMap.isEmpty()) {
+                highestPriorityTasks.clear();
+                highestPriorityTasks.addAll(dependedTasksSet);
                 dependedTasksSet.clear();
                 for (Set dependedValue : uniqueTasksMap.values()) {
                     dependedTasksSet.addAll(dependedValue);
                 }
+                highestPriorityTasks.removeAll(dependedTasksSet);
             }
-
-            //Выводим на консоль задачи с высшим приоритетом
-            System.out.println(highestPriorityTasks);
-            highestPriorityTasks.clear();
         }
         System.out.println(dependedTasksSet);
         timeSpent = System.currentTimeMillis() - startTime;
--- src/main/java/PlannerDemo.java
+++ src/main/java/PlannerDemo.java
@@ -1,7 +1,6 @@
 public class PlannerDemo {
     public static void main(String[] args) {
         Planner test = new Planner();
-        test.planTasks(".\\src\\main\\resources\\data3.txt"); //для Windows
-        //test.planTasks("./src/main/resources/data1.txt"); //для Linux
+        test.planTasks("./src/main/resources/data3.txt");
     }
 }
