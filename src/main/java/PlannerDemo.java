b85c4622e9ddf8c68f876c23b5d93526308abb8f
Aleksandr Matjushenko <guevara3647@gmail.com>
2018-07-17 00:39:08 +0300

    Планировщик заданий. Version 1.2

--- src/main/java/PlannerDemo.java
+++ src/main/java/PlannerDemo.java
@@ -1,6 +1,6 @@
 public class PlannerDemo {
     public static void main(String[] args) {
         Planner test = new Planner();
-        test.planTasks("./src/main/resources/data3.txt");
+        test.planTasks("./src/main/resources/data1.txt");
     }
 }
